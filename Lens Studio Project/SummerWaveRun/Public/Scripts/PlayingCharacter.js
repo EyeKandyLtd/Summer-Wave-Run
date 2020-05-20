b9a5b03cac35e7a64a833583b6e6a952cdafc16e
Isaac Dart <sourcetree@mrdart.com>
2020-05-20 20:30:42 +0100

    Added tilt to player

--- Lens Studio Project/SummerWaveRun/Public/Scripts/PlayingCharacter.js
+++ Lens Studio Project/SummerWaveRun/Public/Scripts/PlayingCharacter.js
@@ -4,17 +4,20 @@
 //@input float strafeSpeed = 600.0 {"widget":"slider", "min":50.0, "max":3000.0}
 //@input float turnStickiness = 0.03 {"widget":"slider", "min":0.0001, "max":0.99}
 //@input float horizonalMovementRange = 100 {"widget":"slider", "min":0, "max":200}
+//@input float tiltWeight = 3
+//@input float tiltMaxDegrees = 0.15 {"widget":"slider", "min":0.02, "max":4}
 //@input float waveBounceSeconds = 4 {"widget":"slider", "min":0.3, "max":32}
 
 
 if(!script.worldController )
 { throw new Error("One or more fields aren't set."); return; }  // Check to prevent Studio lens failure to let you set null fields when in error
 
-var screenTransf = script.getSceneObject().getTransform();
+var playerTransf = script.getSceneObject().getTransform();
 //var imageComp = 
 var facingDirection = -1; // starts facing left
 
 var initialPos;
+var initialRot;
 
 var markedForPositionReset = false; // because it has to be done in Update
 
@@ -23,7 +26,8 @@ var halfExtentXMovementRange = script.horizonalMovementRange * 0.5
 var event = script.createEvent("TurnOnEvent");
 event.bind(function (eventData)
 {
-  initialPos = screenTransf.getLocalPosition();
+    initialPos = playerTransf.getLocalPosition();
+    initialRot = playerTransf.getLocalRotation();
     
 });
 
@@ -34,14 +38,15 @@ var updateEvent = script.createEvent("UpdateEvent");
 updateEvent.bind(function(eventData) {
       
 
-  var pos = screenTransf.getLocalPosition();
+  var pos = playerTransf.getLocalPosition();
+  var rot = playerTransf.getLocalRotation().toEulerAngles();
 
   // reset position to center
   if(markedForPositionReset)
   {
     markedForPositionReset = false;
     pos = initialPos;
-    screenTransf.position = pos;
+    playerTransf.position = pos;
     return;
   }
 
@@ -49,9 +54,8 @@ updateEvent.bind(function(eventData) {
   var tiltSide = script.worldController.api.GetHeadTiltSide();
 
     
-    
+  //set position
   if(tiltSide < -script.turnStickiness) {
-        print("script.strafeSpeed = " + script.strafeSpeed)
     pos.x -= getDeltaTime() *  -(tiltSide * script.strafeSpeed);
     pos.x = Math.max(pos.x, -halfExtentXMovementRange);
     facingDirection = -1;
@@ -64,10 +68,25 @@ updateEvent.bind(function(eventData) {
     //imageComp.flipX = true;
   }
   pos.y = initialPos.y + 3 *  Math.sin(getTime()*script.waveBounceSeconds); // wavy oscillation
+
+  var playerSpeedThisFrame = playerTransf.getLocalPosition().x - pos.x;
     
+  playerTransf.setLocalPosition(pos);
+      
+    
+  //set the lean
+  var targetZrot = initialRot.z + playerSpeedThisFrame * script.tiltWeight;
+  targetZrot = Math.max(targetZrot, initialRot.z - script.tiltMaxDegrees) //ensure degree is above minimum tilt rotation
+  targetZrot = Math.min(targetZrot, initialRot.z + script.tiltMaxDegrees) //ensure degree is below maximum tilt rotation
   
+  var targetRot = new vec3(rot.x, rot.y, targetZrot );
+    rot=targetRot;
+  //rot = vec3.lerp(rot, targetRot, getDeltaTime() * 5.0);
+  playerTransf.setLocalRotation(quat.fromEulerVec(rot));    
     
-  screenTransf.setLocalPosition(pos);
+    
+    
+    
 });
 
 // --- API ---
