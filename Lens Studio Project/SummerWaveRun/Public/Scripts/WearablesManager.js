a64c8e3a242f2abeeb65779de3cb03dc356b81a9
Isaac Dart <sourcetree@mrdart.com>
2020-05-29 13:43:08 +0100

    Wearables logic

---
+++ Lens Studio Project/SummerWaveRun/Public/Scripts/WearablesManager.js
@@ -0,0 +1,51 @@
+// -----JS CODE-----
+//@input SceneObject[] LevelGlasses
+//@input SceneObject[] LevelHats
+
+
+script.api.ActiveRandomHat = function () {
+    RandomActivateOneObject(script.LevelHats);
+};
+
+script.api.ActiveRandomGlasses = function () {
+    RandomActivateOneObject(script.LevelGlasses);
+};
+
+script.api.ActiveLevelWearables = function (levelIndex) {
+    ActiveObject(script.LevelGlasses, levelIndex);
+    ActiveObject(script.LevelHats, levelIndex);
+};
+
+script.api.ActiveRandomWearables = function () {
+    RandomActivateOneObject(script.LevelGlasses);
+    RandomActivateOneObject(script.LevelHats);
+};
+
+
+function RandomActivateOneObject(sceneObjs) {
+    
+    var len = sceneObjs.length;
+
+    if (len > 0) {
+        var randomIndex = Math.floor(Math.random() * len);
+       ActiveObject(sceneObjs, randomIndex);
+    } else {
+        print("ObjectSpawner:: Prefab pool was empty. Please check and restart Lens.");                    
+    }
+        
+}
+
+function ActiveObject(sceneObjs, indx) {
+    
+    var len = sceneObjs.length;
+    
+     for (i = 0; i < len; i++) {
+            
+            if (sceneObjs[i] != null) {
+                sceneObjs[i].enabled == indx;
+            } else {
+                print("ObjectSpawner:: A slot on the prefab pool was null. Please check and restart Lens.");            
+            }
+        }
+    
+}
